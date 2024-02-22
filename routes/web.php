8ddae2e10730667c8ed84d5972ac741783a0f3d6
Galihwahyu Arofah <wahyuuarofah@gmail.com>
2024-02-22 16:02:04 +0700

    Basic/Route Paramaters/Optional Route

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,51 @@
 
 use Illuminate\Support\Facades\Route;
 
+
+
+
+//--OPTIONAL PARAMATERS--
+//Route::get('/user/{name?}', function ($name='Jony'){
+        //return 'Nama saya '.$name;
+//});
+
+
+//Route::get('/user/{name?}', function ($name=null){
+    //return 'Nama saya '.$name;
+//});
+
+
+//--ROUTE PARAMATERS--
+//Route::get('/artikel/{id}', function($id){
+    //return 'Halaman Artikel dengan ID 1';
+//});
+
+//Route::get('/posts/{post}/comments/{comment}', function ($postId, $commentId) {
+    //return 'Pos ke-'.$postId." Komentar ke-: ".$commentId;
+//});
+
+//Route::get('/user/{name}', function ($name){
+    //return 'Nama Saya '.$name;
+//});
+
+
+//--BASIC ROUTE--
+//Route::get('/about', function(){
+    //return '2141762095 / Galihwahyu ';
+//});
+
+//Route::get('/', function(){
+    //return 'Selamat Datang';
+//});
+
+///Route::get('/world', function(){
+   // return 'World';
+//});
+
+
+//Route::get('/hello', function (){
+    //return 'Hello World';
+//});
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,6 +58,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+//Route::get('/', function () {
+    //return view('welcome');
+//});
